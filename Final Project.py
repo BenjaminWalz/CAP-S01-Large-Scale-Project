0584829a577d167a8a0f6d32eeb2ec34d6fbda86
BenjaminWalz <92549207+BenjaminWalz@users.noreply.github.com>
2022-01-04 13:08:30 -0600

    fourth commit
    made the block move like the snake game and changed the colors of the screen. next is to make the walls stop and add the food.

--- Final Project.py
+++ Final Project.py
@@ -12,14 +12,16 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-
-
+TEAL = (0, 128, 128)
+GRASS_GREEN = (126, 200, 80)
 # Mechanics
 WIDTH = 20
 HEIGHT = 20
-VEL = 5
-x = 50
-y = 50
+x_vel = 0
+y_vel = 0
+x = 240
+y = 240
+speed = 10
 
 
 # Functions
@@ -34,7 +36,11 @@ y = 50
 # Initialization
 pygame.init()
 screen = pygame.display.set_mode((500, 500))
+clock = pygame.time.Clock()
+clock.tick(30)
 
+
+# Loop
 run = True
 
 while run == True:
@@ -44,32 +50,27 @@ while run == True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
- 
-    keys = pygame.key.get_pressed()
-      
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_vel = -speed
+                y_vel = 0
+            elif event.key == pygame.K_RIGHT:
+                x_vel = speed
+                y_vel = 0
+            elif event.key == pygame.K_UP:
+                y_vel = -speed
+                x_vel = 0
+            elif event.key == pygame.K_DOWN:
+                y_vel = speed
+                x_vel = 0
 
-    if keys[pygame.K_LEFT] and x>0:
-          x -= VEL
-          
-
-    if keys[pygame.K_RIGHT] and x<500-WIDTH:
-        x += VEL
+        
+    x += x_vel
+    y += y_vel
          
-  
-    if keys[pygame.K_UP] and y>0:
-        y -= VEL
-          
-  
-    if keys[pygame.K_DOWN] and y<500-HEIGHT:
-        # increment in y co-ordinate
-        y += VEL
-         
-              
-    screen.fill((BLACK))
-      
-
-    pygame.draw.rect(screen, (255, 0, 0), (x, y, WIDTH, HEIGHT))
-    
+    screen.fill((GRASS_GREEN))
+    pygame.draw.rect(screen, (TEAL), (x, y, WIDTH, HEIGHT))
     pygame.display.update() 
-
+    
+    clock.tick(30)
 pygame.quit()
