6ff39b6c1330a7073888b5e4c25800c2acb81a1f
BenjaminWalz <92549207+BenjaminWalz@users.noreply.github.com>
2022-01-12 11:42:56 -0600

    sixth commit
    Made the food system move around and work as intended.

--- Final Project.py
+++ Final Project.py
@@ -16,17 +16,19 @@ BLACK = (0, 0, 0)
 TEAL = (0, 128, 128)
 GRASS_GREEN = (126, 200, 80)
 # Mechanics
-SIZE = 10
+SIZE = 50
 x_vel = 0
 y_vel = 0
 x = 240
 y = 240
 speed = 10
-s_width = 500
-s_height = 500
+s_width = 750
+s_height = 750
 rand_x = round(random.randrange(0, s_width - SIZE) / 10) * 10
 rand_y = round(random.randrange(0, s_height - SIZE) / 10) * 10
 score = 0
+print(rand_x)
+print(rand_y)
 
 
 # Initialization
@@ -35,29 +37,28 @@ screen = pygame.display.set_mode((s_width, s_height))
 pygame.display.set_caption('Snake')
 clock = pygame.time.Clock()
 clock.tick(30)
-font_style = pygame.font.SysFont(None, 50)
 
+font_style = pygame.font.SysFont(None, 50)
+score_font = pygame.font.SysFont("comicsansms", 35)##
 
 # Functions
-def message(msg, colour):
+def message(msg, colour, pos_x, pos_y):
     mesg = font_style.render(msg, True, colour)
-    screen.blit(mesg, [s_width/2, s_height/2])
+    screen.blit(mesg, [pos_x, pos_y])
 
 
 def food():
     pygame.draw.rect(screen, (RED), (rand_x, rand_y, SIZE, SIZE))
-
-
-
-
+    
 
 # Loop
 run = True
 
 while run == True:
     
+    
     pygame.time.delay(10)
-  
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -74,24 +75,32 @@ while run == True:
             elif event.key == pygame.K_DOWN:
                 y_vel = speed
                 x_vel = 0
-            if x == rand_x and y == rand_y:
-                run = False
-    
+
+
     if x <= 0 or x >= s_width or y <= 0 or y >= s_height:
         run = False
+    
     if x == rand_x and y == rand_y:
         print("Yummy")
-    
+        score = int(score)
+        score += 1
+        print(score)
+        rand_x = round(random.randrange(0, s_width - SIZE) / 10) * 10
+        rand_y = round(random.randrange(0, s_height - SIZE) / 10) * 10
+ 
     x += x_vel
     y += y_vel
     screen.fill((GRASS_GREEN))
-    food()
+    
+    pygame.draw.rect(screen, (RED), (rand_x, rand_y, SIZE, SIZE))
+
+    message('Score: ' + str(score), WHITE, 5, 0)
     pygame.draw.rect(screen, (TEAL), (x, y, SIZE, SIZE))
     pygame.display.update() 
     
-    clock.tick(30)
+    clock.tick(speed * 2)
 
-message('You Lost', RED)
+message('You Lost', RED, 500, 250)
 pygame.display.update()
 time.sleep(1)
 pygame.quit()
